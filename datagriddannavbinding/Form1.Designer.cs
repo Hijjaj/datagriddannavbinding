76b03113658704a89c4d3ab376889f980d5b3436
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2022-05-25 14:32:08 +0700

    Tampilan 3 form

--- datagriddannavbinding/Form1.Designer.cs
+++ datagriddannavbinding/Form1.Designer.cs
@@ -93,7 +93,7 @@ namespace datagriddannavbinding
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(306, 209);
+            this.button1.Location = new System.Drawing.Point(312, 209);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
